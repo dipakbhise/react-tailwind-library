faa5c35dc402abea87f960e609193e652c7d5626
Dipak <dipakbhise420@gmail.com>
2022-12-08 11:12:45 +0530

    added combobox and multiselect

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,6 @@
-import { CloseIcon, RightIcon } from "../icons/Icons";
+
 import Button from "./components/Button";
 import Combobox from "./components/Combobox";
 
 
-export {Button, Combobox, CloseIcon, RightIcon};
+export {Button, Combobox};
